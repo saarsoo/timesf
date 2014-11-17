ed14a3eb0c9abcd3b38661dac94c4e0d34b2b428
saarsoo <johan@saarsoo.se>
2014-11-17 19:50:12 +0100

    Updated ajax.js

--- qml/ajax.js
+++ qml/ajax.js
@@ -1,12 +1,18 @@
-function get(url, callback){
+function get(url, callback, options){
     var http = new XMLHttpRequest();
     http.open("GET", url, true);
 
-    // Send the proper header information along with the request
-    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    http.setRequestHeader("Connection", "close");
+    if (options) {
+        if (options.header) {
+            for (var name in options.header) {
+                if (options.header.hasOwnProperty(name)){
+                    http.setRequestHeader(name, options.header[name]);
+                }
+            }
+        }
+    }
 
-    http.onreadystatechange = function() { // Call a function when the state changes.
+    http.onreadystatechange = function() {
         if (http.readyState == 4) {
             if (http.status == 200) {
                 callback(http.responseText);
@@ -17,4 +23,17 @@ function get(url, callback){
     }
 
     http.send();
+}
+
+function post(url, params, callback) {
+    var http = new XMLHttpRequest();
+    http.open("POST", url, true);
+
+    http.onreadystatechange = function() {
+        if (http.readyState == 4) {
+            callback(http.responseText, http);
+        }
+    }
+
+    http.send(params);
 }
