0e53e60a012750a30708ef5f2608186738f2f010
saarsoo <johan@saarsoo.se>
2014-11-17 17:58:13 +0100

    Fixed some translations

--- translations/timesf-de.ts
+++ translations/timesf-de.ts
@@ -1,30 +1,15 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.0">
 <context>
     <name>CoverPage</name>
     <message>
         <source>My Cover</source>
-        <translation type="vanished">Mein Cover</translation>
+        <translation type="obsolete">Mein Cover</translation>
     </message>
     <message>
         <source>Timesf</source>
         <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>FirstPage</name>
-    <message>
-        <source>Show Page 2</source>
-        <translation type="vanished">Zur Seite 2</translation>
-    </message>
-    <message>
-        <source>UI Template</source>
-        <translation type="vanished">UI-Vorlage</translation>
-    </message>
-    <message>
-        <source>Hello Sailors</source>
-        <translation type="vanished">Hallo Matrosen</translation>
     </message>
 </context>
 <context>
@@ -36,17 +21,6 @@
     <message>
         <source>No logs today!</source>
         <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>SecondPage</name>
-    <message>
-        <source>Nested Page</source>
-        <translation type="vanished">Unterseite</translation>
-    </message>
-    <message>
-        <source>Item</source>
-        <translation type="vanished">Element</translation>
     </message>
 </context>
 <context>
