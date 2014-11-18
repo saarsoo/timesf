1871e726cd134fda7ffff2f526f6fc221977e47c
Johan Saarsoo <johan@saarsoo.se>
2014-11-18 20:56:55 +0100

    Refactored ajax.js

--- qml/ajax.js
+++ qml/ajax.js
@@ -1,39 +1,46 @@
-function get(url, callback, options){
-    var http = new XMLHttpRequest();
-    http.open("GET", url, true);
+function request(options){
+    options = options || {};
+    options.async = options.async || true;
+    options.headers = options.headers || {};
 
-    if (options) {
-        if (options.header) {
-            for (var name in options.header) {
-                if (options.header.hasOwnProperty(name)){
-                    http.setRequestHeader(name, options.header[name]);
+    var http = new XMLHttpRequest();
+    http.open(method, url, async);
+
+    for (var name in options.header) {
+        if (options.header.hasOwnProperty(name)) {
+            http.setRequestHeader(name, options.header[name]);
+        }
+    }
+
+    http.onreadystatechange = function(){
+        if (http.readyState == 4) {
+            if (http.status == 200 || http.status == 302) {
+                if (options.success) {
+                    options.success();
+                }
+            } else {
+                if (options.error) {
+                    options.error();
                 }
             }
         }
     }
 
-    http.onreadystatechange = function() {
-        if (http.readyState == 4) {
-            if (http.status == 200) {
-                callback(http.responseText);
-            } else {
-                console.log("error: " + http.status);
-            }
-        }
-    }
-
-    http.send();
+    http.send(options.data);
 }
 
-function post(url, params, callback) {
-    var http = new XMLHttpRequest();
-    http.open("POST", url, true);
+function get(url, success){
+    request({
+                method: "GET",
+                url: url,
+                success: success
+            });
+}
 
-    http.onreadystatechange = function() {
-        if (http.readyState == 4) {
-            callback(http.responseText, http);
-        }
-    }
-
-    http.send(params);
+function post(url, data, success) {
+    request({
+                url: url,
+                data: data,
+                success: success
+            });
 }
