55560e2badbf9dd228bd1b78ca2cda9218237489
Johan Saarsoo <johan@saarsoo.se>
2014-11-18 21:29:39 +0100

    Refactored ajax.js

--- qml/ajax.js
+++ qml/ajax.js
@@ -2,36 +2,39 @@ function request(options){
     options = options || {};
     options.async = options.async || true;
     options.headers = options.headers || {};
+    options.method = options.method || 'GET';
 
-    var http = new XMLHttpRequest();
-    http.open(method, url, async);
+    var xhr = new XMLHttpRequest();
+    xhr.open(options.method, options.url, options.async);
 
     for (var name in options.header) {
         if (options.header.hasOwnProperty(name)) {
-            http.setRequestHeader(name, options.header[name]);
+            xhr.setRequestHeader(name, options.header[name]);
         }
     }
 
-    http.onreadystatechange = function(){
-        if (http.readyState == 4) {
-            if (http.status == 200 || http.status == 302) {
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState == 4) {
+            if (xhr.status == 200 || xhr.status == 302) {
                 if (options.success) {
-                    options.success();
+                    options.success(http.responseText);
                 }
             } else {
                 if (options.error) {
-                    options.error();
+                    options.error(http.responseText);
                 }
             }
         }
     }
 
-    http.send(options.data);
+    xhr.send(options.data);
+
+    return xhr;
 }
 
 function get(url, success){
     request({
-                method: "GET",
+                method: 'GET',
                 url: url,
                 success: success
             });
